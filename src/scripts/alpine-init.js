b07f9bccf73b9f82917443f63dfe88a846ce63c5
pedregalux <felipe.perry@gmail.com>
2026-06-17 23:37:57 -0400

    fix: carga Alpine.js desde archivo para menú hamburguesa responsive

--- src/scripts/alpine-init.js
+++ src/scripts/alpine-init.js
@@ -2,3 +2,8 @@ import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 Alpine.start();
+
+document.addEventListener('astro:after-swap', () => {
+  Alpine.destroyTree(document.body);
+  Alpine.initTree(document.body);
+});
